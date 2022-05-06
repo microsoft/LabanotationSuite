ed3766ffdf8dc1c567f9a1a7b16979c046b95e43
Naoki Wake <naoki.wake@microsoft.com>
2022-05-06 14:54:41 -0700

    add comment

--- GestureAuthoringTools/LabanEditor/docker/README.md
+++ GestureAuthoringTools/LabanEditor/docker/README.md
@@ -4,3 +4,6 @@ docker build -t labansuite:latest .
 docker run -ti --rm  -e DISPLAY=<hostip:display> labansuite:latest
 example of <hostip:display>
 192.168.80.1:0.0
+
+# Run docker in windows (please set DISPLAY=<> in run.bats)
+\run.bat [csv path] [--output dir]
--- GestureAuthoringTools/LabanEditor/docker/run.bat
+++ GestureAuthoringTools/LabanEditor/docker/run.bat
@@ -24,6 +24,6 @@ if "%2"=="" (
 docker run -it --rm ^
 -v "%CSV_P%:/tmp_in/" ^
 -v "%DIR%:/tmp/" ^
--e DISPLAY=192.168.80.1:0.0 ^
+-e DISPLAY=172.23.160.1:0.0 ^
 labansuite:latest ^
-/bin/bash -c "source activate labansuite && python main.py --alg parallel --inputfile /tmp_in/%CSV% --outputfolder /tmp/ --nogui"
+/bin/bash -c "source activate labansuite && python main.py --alg parallel --inputfile /tmp_in/%CSV% --outputfolder /tmp/"
